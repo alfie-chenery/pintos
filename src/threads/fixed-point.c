27fdd681a8f6bffed8e369d753a4285c2e8b2ee4
Dhruv Saraff <ds3520@ic.ac.uk>
2021-10-29 02:18:25 +0100

    Used fp1714 alias in all appropriate places

--- src/threads/fixed-point.c
+++ src/threads/fixed-point.c
@@ -5,54 +5,54 @@ fp1714 from_integer (int32_t n)
     return n * FIXED_POINT_SHIFT;
 }
 
-int32_t to_integer_round_0 (int32_t x)
+int32_t to_integer_round_0 (fp1714 x)
 {
     return x / FIXED_POINT_SHIFT;
 }
 
-int32_t to_integer_round_nearest (int32_t x)
+int32_t to_integer_round_nearest (fp1714 x)
 {
     return x >= 0 ? (x + FIXED_POINT_SHIFT / 2) / FIXED_POINT_SHIFT : 
         (x - FIXED_POINT_SHIFT / 2) / FIXED_POINT_SHIFT;
 }
 
-fp1714 add_x_and_y (int32_t x, int32_t y)
+fp1714 add_x_and_y (fp1714 x, fp1714 y)
 {
     return x + y;
 }
 
-fp1714 subtract_y_from_x (int32_t x, int32_t y)
+fp1714 subtract_y_from_x (fp1714 x, fp1714 y)
 {
     return x - y;
 }
 
-fp1714 add_x_and_n (int32_t x, int32_t n)
+fp1714 add_x_and_n (fp1714 x, int32_t n)
 {
     return x + n * FIXED_POINT_SHIFT;
 }
 
-fp1714 subtract_n_from_x (int32_t x, int32_t n)
+fp1714 subtract_n_from_x (fp1714 x, int32_t n)
 {
     return x - n * FIXED_POINT_SHIFT;
 }
 
-fp1714 multiply_x_and_y (int32_t x, int32_t y)
+fp1714 multiply_x_and_y (fp1714 x, fp1714 y)
 {
     return ((int64_t) x) * y / FIXED_POINT_SHIFT;
 }
 
-fp1714 multiply_x_and_n (int32_t x, int32_t n)
+fp1714 multiply_x_and_n (fp1714 x, int32_t n)
 {
     return x * n;
 }
 
-fp1714 divide_x_and_y (int32_t x, int32_t y)
+fp1714 divide_x_and_y (fp1714 x, fp1714 y)
 {
     return ((int64_t)x) * FIXED_POINT_SHIFT / y;
 }
 
 
-fp1714 divide_x_and_n (int32_t x, int32_t n)
+fp1714 divide_x_and_n (fp1714 x, int32_t n)
 {
     return x / n;
 }
--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -10,15 +10,15 @@
 typedef int32_t fp1714;
 
 fp1714 from_integer (int32_t n);
-int32_t to_integer_round_0 (int32_t x);
-int32_t to_integer_round_nearest (int32_t x);
-fp1714 add_x_and_y (int32_t x, int32_t y);
-fp1714 subtract_y_from_x (int32_t x, int32_t y);
-fp1714 add_x_and_n (int32_t x, int32_t n);
-fp1714 subtract_n_from_x (int32_t x, int32_t y);
-fp1714 multiply_x_and_y (int32_t x, int32_t y);
-fp1714 multiply_x_and_n (int32_t x, int32_t n);
-fp1714 divide_x_and_y (int32_t x, int32_t y);
-fp1714 divide_x_and_n (int32_t x, int32_t n);
+int32_t to_integer_round_0 (fp1714 x);
+int32_t to_integer_round_nearest (fp1714 x);
+fp1714 add_x_and_y (fp1714 x, fp1714 y);
+fp1714 subtract_y_from_x (fp1714 x, fp1714 y);
+fp1714 add_x_and_n (fp1714 x, int32_t n);
+fp1714 subtract_n_from_x (fp1714 x, int32_t y);
+fp1714 multiply_x_and_y (fp1714 x, fp1714 y);
+fp1714 multiply_x_and_n (fp1714 x, int32_t n);
+fp1714 divide_x_and_y (fp1714 x, fp1714 y);
+fp1714 divide_x_and_n (fp1714 x, int32_t n);
 
 #endif
